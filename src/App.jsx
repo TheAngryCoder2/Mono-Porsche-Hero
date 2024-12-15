77e20d2a352c494b6a57fdc2821ad8550d6992a6
The Angry Coder <79235452+TheAngryCoder2@users.noreply.github.com>
2024-12-15 17:19:38 +0000

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-
+import { useState } from "react";
+import Background from './Components/Background/Background';
 
 const App = () => {
+  let heroData = [
+    {text1:"Change is easy.", text2:"Improvement is far more difficult."},
+    {text1:"I couldn't find the sports car of my dreams.", text2:"So i build it myself"},
+    {text1:"Good design.", text2:"Should be honest."},
+    {text1:"If one does not fail at times.", text2:"Then one has not challenged himself."},
+    {text1:"Porsche...", text2:"There is no, substitute."},
+  ]
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
   return (
-    <div>App</div>
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+    </div>
   )
 }
 
--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,9 +1,32 @@
 import './Background.css'
+import image1 from '../../assets/149740B68C0F4AC8BB4D99B5A52D7AB4.webp'
+import image2 from '../../assets/525CF14DB8D744ED87C781DFD550EC37_D9081EA4AB3D43AB889A9E9B9A5B056B_00X_Easy-Model-Selector_13_CL16Z20IX0001_1_GL_5_175_rgb.avif'
+import image3 from '../../assets/C89D17520DED4146BA058746BEAF3502.webp'
+import image4 from '../../assets/CA141E55F8C241A99AF3501BFDF3FF90.webp'
+import image5 from '../../assets/F37C21DB85FB4547A771546059EAE61B.webp'
 
-const Background = () => {
-  return (
-    <div>Background</div>
-  )
+const Background = ({playStatus, heroCount}) => {
+  if (heroCount === 0)
+  {
+    return <img src={image1} className='background' alt='' />
+  }
+  else if (heroCount === 1)
+    {
+      return <img src={image2} className='background' alt='' />
+    }
+  else if (heroCount === 2)
+    {
+      return <img src={image3} className='background' alt='' />
+    }
+  else if (heroCount === 3)
+    {
+      return <img src={image4} className='background' alt='' />
+    }
+  else if (heroCount === 4)
+    {
+      return <img src={image5} className='background' alt='' />
+    }
+
 }
 
 export default Background
